f13f21e39e49a001ad32b4bbf5016da0fb77d214
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2023-11-21 19:22:51 -0800

    Use rsopt develop branch for install
    As suggested in an email from @cchall, use the 2023-11-21 version in the
    development branch of rsopt, which does not need to import pymeshlab and
    pymoab in sirepo.
    
    sirepo then no longer needs to be reverted.

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,8 +5,5 @@ poetry install
 
 # install rsopt into virtual environment. This must be done separately because
 # pykern needs to be available to build rsopt
-poetry run pip install "rsopt[full] @ git+https://github.com/radiasoft/rsopt@c176a36"
-
-# revert sirepo (installed by rsopt) to an older version that doesn't require pymoab 
-# and pymeshlab
-poetry run pip install "sirepo @ git+https://github.com/radiasoft/sirepo.git@013784b70e06fa27f9f346d557a659bf99e4f850"
+# rsopt@develop 2023-11-21
+poetry run pip install "rsopt[full] @ git+https://github.com/radiasoft/rsopt@76ab6211eed33675eda91930666a0e83d96f568c"
