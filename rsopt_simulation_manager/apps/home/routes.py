cdca1595c31564b6d9af2b5a4dbf5bebee23832d
Reinier van Mourik <reiniervanmourik@outlook.com>
2023-12-06 14:37:33 -0800

    Add 404 and 500 error handlers

--- rsopt_simulation_manager/apps/home/routes.py
+++ rsopt_simulation_manager/apps/home/routes.py
@@ -60,38 +60,13 @@ def list_runs(datetime_str = None):
     else:
         return render_template('home/page-404.html'), 404
 
-# @blueprint.route('/<template>')
-# def route_template(template):
 
-#     try:
+@blueprint.app_errorhandler(404) 
+def not_found(e): 
+  # defining function 
+  return render_template("home/page-404.html"), 404
 
-#         if not template.endswith('.html'):
-#             template += '.html'
-
-#         # Detect the current page
-#         segment = get_segment(request)
-
-#         # Serve the file (if exists) from app/templates/home/FILE.html
-#         return render_template("home/" + template, segment=segment)
-
-#     except TemplateNotFound:
-#         return render_template('home/page-404.html'), 404
-
-#     except:
-#         return render_template('home/page-500.html'), 500
-
-
-# Helper - Extract current page name from request
-def get_segment(request):
-
-    try:
-
-        segment = request.path.split('/')[-1]
-
-        if segment == '':
-            segment = 'index'
-
-        return segment
-
-    except:
-        return None
+@blueprint.app_errorhandler(500) 
+def unspecified_error(e): 
+  # defining function 
+  return render_template("home/page-500.html"), 500
