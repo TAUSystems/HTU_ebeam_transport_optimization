f1e2a64255cb9858328ef12bfccdc86a84574914
Christopher Doss <cedoss@lbl.gov>
2023-12-13 16:56:10 -0800

    Initialize with binary data
    using the numpy array generated by reading the .proc.plainbin file

--- shared/phase_space_tools/Beam.py
+++ shared/phase_space_tools/Beam.py
@@ -3,6 +3,7 @@ Quick classes to store an electron beam's 6D phase space
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def calculate_geometric_emittance(x, xp):
@@ -18,38 +19,38 @@ class Beam:
     """
     The coordinates are defined as follows:
     x, y, z: [m]
-    px, py:  [rad]
+    xp, yp:  [rad]
     pz:      [eV]
     """
 
     def __init__(self, n_particles):
         self.n_particles = n_particles
         self.x = np.zeros(n_particles)
-        self.px = np.zeros(n_particles)
+        self.xp = np.zeros(n_particles)
         self.y = np.zeros(n_particles)
-        self.py = np.zeros(n_particles)
+        self.yp = np.zeros(n_particles)
         self.z = np.zeros(n_particles)
         self.pz = np.zeros(n_particles)
 
-    def set_phase_space(self, x, px, y, py, z, pz):
+    def set_phase_space(self, x, xp, y, yp, z, pz):
         self.set_x(x)
-        self.set_px(px)
+        self.set_xp(xp)
         self.set_y(y)
-        self.set_py(py)
+        self.set_yp(yp)
         self.set_z(z)
         self.set_pz(pz)
 
     def calculate_x_emittance_n(self):
-        emit_x = calculate_geometric_emittance(self.x, self.px)
+        emit_x = calculate_geometric_emittance(self.x, self.xp)
         return emit_x * self.calculate_gamma_lorentz()
 
     def calculate_y_emittance_n(self):
-        emit_y = calculate_geometric_emittance(self.y, self.py)
+        emit_y = calculate_geometric_emittance(self.y, self.yp)
         return emit_y * self.calculate_gamma_lorentz()
 
     def calculate_gamma_lorentz(self):
         electron_rest_mass = 0.511e6  # eV
-        beam_gamma = self.pz/electron_rest_mass
+        beam_gamma = self.pz / electron_rest_mass
         return np.average(beam_gamma)
 
     def calculate_average_energy_eV(self):
@@ -57,7 +58,7 @@ class Beam:
 
     def calculate_energy_spread(self):
         average_energy = self.calculate_average_energy_eV()
-        return np.sqrt(np.average(np.square(self.pz - average_energy)))/average_energy * 100
+        return np.sqrt(np.average(np.square(self.pz - average_energy))) / average_energy * 100
 
     def calculate_x_average(self):
         return np.average(self.x)
@@ -65,51 +66,51 @@ class Beam:
     def calculate_y_average(self):
         return np.average(self.y)
 
-    def calculate_px_average(self):
-        return np.average(self.px)
+    def calculate_xp_average(self):
+        return np.average(self.xp)
 
-    def calculate_py_average(self):
-        return np.average(self.py)
+    def calculate_yp_average(self):
+        return np.average(self.yp)
 
     def calculate_x_beta(self):
         sigma_x2 = np.average(np.square(self.x - self.calculate_x_average()))
-        return sigma_x2/calculate_geometric_emittance(self.x, self.px)
+        return sigma_x2 / calculate_geometric_emittance(self.x, self.xp)
 
     def calculate_y_beta(self):
         sigma_y2 = np.average(np.square(self.y - self.calculate_y_average()))
-        return sigma_y2/calculate_geometric_emittance(self.y, self.py)
+        return sigma_y2 / calculate_geometric_emittance(self.y, self.yp)
 
     def calculate_x_gamma(self):
-        sigma_xp2 = np.average(np.square(self.px - self.calculate_px_average()))
-        return sigma_xp2 / calculate_geometric_emittance(self.x, self.px)
+        sigma_xp2 = np.average(np.square(self.xp - self.calculate_xp_average()))
+        return sigma_xp2 / calculate_geometric_emittance(self.x, self.xp)
 
     def calculate_y_gamma(self):
-        sigma_yp2 = np.average(np.square(self.py - self.calculate_py_average()))
-        return sigma_yp2 / calculate_geometric_emittance(self.y, self.py)
+        sigma_yp2 = np.average(np.square(self.yp - self.calculate_yp_average()))
+        return sigma_yp2 / calculate_geometric_emittance(self.y, self.yp)
 
     def calculate_x_alpha(self):
         dx = self.x - self.calculate_x_average()
-        dxp = self.px - self.calculate_px_average()
+        dxp = self.xp - self.calculate_xp_average()
         sigma_xxp = -np.average(dx * dxp)
-        return sigma_xxp / calculate_geometric_emittance(self.x, self.px)
+        return sigma_xxp / calculate_geometric_emittance(self.x, self.xp)
 
     def calculate_y_alpha(self):
         dy = self.y - self.calculate_y_average()
-        dyp = self.py - self.calculate_py_average()
+        dyp = self.yp - self.calculate_yp_average()
         sigma_yyp = -np.average(dy * dyp)
-        return sigma_yyp / calculate_geometric_emittance(self.y, self.py)
+        return sigma_yyp / calculate_geometric_emittance(self.y, self.yp)
 
     def set_x(self, x):
         self.x = np.copy(x)
 
-    def set_px(self, px):
-        self.px = np.copy(px)
+    def set_xp(self, xp):
+        self.xp = np.copy(xp)
 
     def set_y(self, y):
         self.y = np.copy(y)
 
-    def set_py(self, py):
-        self.py = np.copy(py)
+    def set_yp(self, yp):
+        self.yp = np.copy(yp)
 
     def set_z(self, z):
         self.z = np.copy(z)
@@ -123,14 +124,14 @@ class Beam:
     def get_x(self):
         return self.x
 
-    def get_px(self):
-        return self.px
+    def get_xp(self):
+        return self.xp
 
     def get_y(self):
         return self.y
 
-    def get_py(self):
-        return self.py
+    def get_yp(self):
+        return self.yp
 
     def get_z(self):
         return self.z
@@ -138,6 +139,44 @@ class Beam:
     def get_pz(self):
         return self.pz
 
+    def plot_transverse_phase_space(self, num_bins=20, scale=1e3):
+        fig, ax = plt.subplots(2, 2, figsize=(9, 7))
+        mycmap = plt.get_cmap('viridis')
+        mycmap.set_under(color='white')  # map 0 to this color
+
+        ax[0, 0].hist2d(self.get_x()*scale, self.get_xp()*scale, bins=num_bins, cmap=mycmap)
+        ax[0, 0].set_xlabel("x [mm]")
+        ax[0, 0].set_ylabel("xp [mrad]")
+
+        ax[1, 0].hist2d(self.get_x()*scale, self.get_yp()*scale, bins=num_bins, cmap=mycmap)
+        ax[1, 0].set_xlabel("x [mm]")
+        ax[1, 0].set_ylabel("yp [mrad]")
+
+        ax[0, 1].hist2d(self.get_y()*scale, self.get_xp()*scale, bins=num_bins, cmap=mycmap)
+        # ax[0, 1].scatter(self.get_y()*scale, self.get_xp()*scale, s=5)
+        ax[0, 1].set_xlabel("y [mm]")
+        ax[0, 1].set_ylabel("xp [mrad]")
+
+        ax[1, 1].hist2d(self.get_y()*scale, self.get_yp()*scale, bins=num_bins, cmap=mycmap)
+        ax[1, 1].set_xlabel("y [mm]")
+        ax[1, 1].set_ylabel("yp [mrad]")
+
+        fig.tight_layout()
+        plt.show()
+
+        return
+
+    def plot_longitudinal_phase_space(self, num_bins=50):
+        plt.figure(figsize=(5, 4))
+        mycmap = plt.get_cmap('viridis')
+        mycmap.set_under(color='white')  # map 0 to this color
+
+        plt.hist2d(self.get_z()*1e6, self.get_pz()/1e6, bins=num_bins, cmap=mycmap)
+        plt.xlabel("z [um]")
+        plt.ylabel("pz [MeV]")
+        plt.show()
+
+        return
 
 class NumpyBeam(Beam):
 
@@ -145,9 +184,9 @@ class NumpyBeam(Beam):
         """
         Initialize a beam using a 6D phase space saved in a .npy file
         :param filename: Filename to .npy file with 6D beam distribution, by default we are assuming that the file is
-                         saved as x,px,y,py,z,pz with pz saved as eV, but some older version are saved otherwise
+                         saved as x,xp,y,yp,z,pz with pz saved as eV, but some older version are saved otherwise
         :param adjust_to_average_energy_MeV: Optional, legacy parameter to convert from fraction offset to eV
-        :param is_alternate_ordering: Optional, legacy flag for older files saved as x,y,z,px,py,pz
+        :param is_alternate_ordering: Optional, legacy flag for older files saved as x,y,z,xp,yp,pz
         """
         beam_data = np.load(filename)
         super().__init__(np.shape(beam_data)[1])
@@ -160,3 +199,24 @@ class NumpyBeam(Beam):
             self.set_phase_space(beam_data[0], beam_data[3], beam_data[1], beam_data[4], beam_data[2], energy_eV)
         else:
             self.set_phase_space(beam_data[0], beam_data[1], beam_data[2], beam_data[3], beam_data[4], energy_eV)
+
+
+class SDDSBeam(Beam):
+
+    def __init__(self, beam_data):
+        """
+        Initialize a beam using a 6D phase space saved in a .npy file
+        :param beam_data: the output of the utils function
+        """
+        super().__init__(np.shape(beam_data)[0])
+
+        gamma = beam_data[:, 5]
+        electron_rest_mass = 0.511e6  # eV
+        energy_eV = gamma * electron_rest_mass
+
+        beam_t = beam_data[:, 4]
+        beam_z = (beam_t - np.average(beam_t)) * 2.998e8
+
+        self.set_phase_space(x=beam_data[:, 0], xp=beam_data[:, 1],
+                             y=beam_data[:, 2], yp=beam_data[:, 3],
+                             z=beam_z, pz=energy_eV)
