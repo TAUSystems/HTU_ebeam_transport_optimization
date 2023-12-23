e4c7dcc2e8c29f60c80e74b342b5880dbfc8ce2e
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2023-12-22 17:00:07 -0800

    Add create experiment functionality

--- rsopt_simulation_manager/apps/logic/experiments.py
+++ rsopt_simulation_manager/apps/logic/experiments.py
@@ -1,10 +1,17 @@
 from __future__ import annotations
+
 from typing import NamedTuple
+from pathlib import Path
+from .settings import load_config
 
 Experiment = NamedTuple("Experiment", name=str, description=str, num_runs=int)
 
-def create_new_experiment(form: dict):
-    print(form)
+def create_new_experiment(form_data: dict):
+    config = load_config()
+
+    experiment_path = Path(config['Directories']['results_path']) / form_data['name']
+    experiment_path.mkdir(parents=True, exist_ok=False)
+    (experiment_path / 'description.txt').write_text(form_data['description'])
 
 def get_experiments() -> list[Experiment]:
     experiment_table_items = [
