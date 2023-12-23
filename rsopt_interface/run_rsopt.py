0db17461679257acfce4bbb089044450de8f09e7
Reinier van Mourik <reinier.vanmourik@tausystems.com>
2023-12-22 17:10:39 -0800

    Add rsopt_interface package
    For other packages to call rsopt.

---
+++ rsopt_interface/run_rsopt.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from pathlib import Path
+import subprocess
+
+def run_rsopt(rsopt_simulation_files_path: Path, results_path: Path):
+    # for now just set status to Success
+    subprocess
+    (results_path / 'status.txt').write_text('success')
+    
